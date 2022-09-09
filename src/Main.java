3784246bc6f8ac07cf092418ba75dbb63378ae0d
Mastore84 <mpb.petersen@gmail.com>
2022-09-09 09:50:49 +0200

    added greet

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         SuperGreeter sg = new SuperGreeter();
         sg.start();
+        sg.greet();
     }
 }
--- src/SuperGreeter.java
+++ src/SuperGreeter.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class SuperGreeter {
     public void start(){
         System.out.println("Det virker!");
+    }
+
+    public void greet(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Indtast dit navn: ");
+        String name = sc.nextLine();
+        System.out.println("Hej " + name + "!");
     }
 }
